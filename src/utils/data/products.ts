a73be5484e050ef8d006beed844370f162f7beba
SamuelCosta22 <samuelgcostainstitucional@gmail.com>
2024-05-09 17:21:13 -0300

    product interface and add function to cart

--- src/utils/data/products.ts
+++ src/utils/data/products.ts
@@ -21,13 +21,32 @@ const MENU = [
           "Molho da casa;",
         ],
       },
+      {
+        id: "2",
+        title: "X-HTML",
+        price: 24.9,
+        description:
+          "A base sólida deste hambúrguer vai deixar você com a sensação de estar sempre estruturado...",
+        cover: require("../../assets/products/cover/4.png"),
+        thumbnail: require("../../assets/products/thumbnail/4.png"),
+        ingredients: [
+          "Pão brioche;",
+          "2x carnes smash (blend da casa) de 80g;",
+          "Queijo cheddar;",
+          "Alface;",
+          "Tomate;",
+          "Picles;",
+          "Cebola;",
+          "Molho da casa;",
+        ],
+      },
     ],
   },
   {
     title: "Promoções",
     data: [
       {
-        id: "2",
+        id: "3",
         title: "X-JavaScript",
         price: 34.9,
         description:
@@ -46,7 +65,7 @@ const MENU = [
         ],
       },
       {
-        id: "3",
+        id: "4",
         title: "X-Cobol",
         price: 32.7,
         description:
@@ -65,7 +84,7 @@ const MENU = [
         ],
       },
       {
-        id: "4",
+        id: "5",
         title: "X-Tailwind",
         price: 29.9,
         description:
@@ -86,10 +105,53 @@ const MENU = [
     ],
   },
   {
+    title: "Outras Opções",
+    data: [
+      {
+        id: "6",
+        title: "X-CSS",
+        price: 29.9,
+        description:
+          "Aquele tão estiloso e original que você vai querer adicionar animações de entrada só para exibi-lo...",
+        cover: require("../../assets/products/cover/1.png"),
+        thumbnail: require("../../assets/products/thumbnail/1.png"),
+        ingredients: [
+          "Pão brioche;",
+          "2x carnes smash (blend da casa) de 80g;",
+          "Queijo cheddar;",
+          "Alface;",
+          "Tomate;",
+          "Picles;",
+          "Cebola;",
+          "Molho da casa;",
+        ],
+      },
+      {
+        id: "7",
+        title: "X-Python",
+        price: 29.9,
+        description:
+          "Aquele tão simples, limpo e gostoso que você vai se perguntar por que não o descobriu antes...",
+        cover: require("../../assets/products/cover/2.png"),
+        thumbnail: require("../../assets/products/thumbnail/2.png"),
+        ingredients: [
+          "Pão brioche;",
+          "2x carnes smash (blend da casa) de 80g;",
+          "Queijo cheddar;",
+          "Alface;",
+          "Tomate;",
+          "Picles;",
+          "Cebola;",
+          "Molho da casa;",
+        ],
+      },
+    ],
+  },
+  {
     title: "Sobremesa",
     data: [
       {
-        id: "5",
+        id: "8",
         title: "Sorvete com Brownie",
         price: 18.9,
         description:
@@ -103,7 +165,7 @@ const MENU = [
         ],
       },
       {
-        id: "6",
+        id: "9",
         title: "Cupcake",
         price: 22.9,
         description:
@@ -112,19 +174,57 @@ const MENU = [
         thumbnail: require("../../assets/products/thumbnail/6.png"),
         ingredients: ["Escolha o sabor;", "Chantilly;"],
       },
+      {
+        id: "10",
+        title: "Java-Pie",
+        price: 18.9,
+        description:
+          "Uma torta tão versátil que pode ser saboreada em qualquer hora do dia, com um toque de café...",
+        cover: require("../../assets/products/cover/9.jpg"),
+        thumbnail: require("../../assets/products/thumbnail/9.jpg"),
+        ingredients: [
+          "1x Brownie;",
+          "1x Bola de sorvete a sua escolha",
+          "Escolha sua calda;",
+        ],
+      },
+      {
+        id: "11",
+        title: "PHP-Sundae",
+        price: 18.9,
+        description:
+          "Um sundae com muitos toppings diferentes, porque por mais que alguns achem difícil, é sempre bom ter muitas opções...",
+        cover: require("../../assets/products/cover/10.png"),
+        thumbnail: require("../../assets/products/thumbnail/10.png"),
+        ingredients: [
+          "1x Brownie;",
+          "1x Bola de sorvete a sua escolha",
+          "Escolha sua calda;",
+        ],
+      },
     ],
   },
   {
     title: "Bebidas",
     data: [
       {
-        id: "7",
+        id: "12",
         title: "Hmmm, coquinha!",
         price: 6.9,
         thumbnail: require("../../assets/products/thumbnail/7.png"),
         cover: require("../../assets/products/cover/7.png"),
         description:
           "Uma coca super gelada para acompanhar o seu super lanche...",
+        ingredients: [],
+      },
+      {
+        id: "13",
+        title: "Guaranazin de lei!",
+        price: 5.9,
+        thumbnail: require("../../assets/products/thumbnail/8.png"),
+        cover: require("../../assets/products/cover/8.png"),
+        description:
+          "Umaa bebida brasileira acompanhar o seu lanche com estilo...",
         ingredients: [],
       },
     ],
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/app/index.tsx', './src/app/_layout.tsx',
-    './src/components/loading.tsx', './src/components/header.tsx', './src/components/category-button.tsx'
+    './src/app/index.tsx', './src/app/_layout.tsx', './src/app/product/[id].tsx',
+    './src/stores/cart-store.ts', './src/stores/helpers/cart-in-memory.ts',
+    './src/components/loading.tsx', './src/components/header.tsx', './src/components/category-button.tsx', './src/components/products.tsx', './src/components/button.tsx', './src/components/link-button.tsx',
   ],
   theme: {
     extend: {
